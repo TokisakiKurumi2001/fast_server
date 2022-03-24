05f3943ba6502c8efa3ed870228ba33c2779198a
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2022-03-24 10:23:03 +0700

    Export JSON object through API

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/sample_backend/")
+def sample_backend_api():
+    data = json.load(open("data/sample_backend_api.json", "r"))
+    return data
